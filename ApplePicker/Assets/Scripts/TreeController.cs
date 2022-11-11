2be82b8c0d5cfb6f13be2df66c1b8603df993156
Valkiria12188 <Valkiria12188@o2.pl>
2022-11-11 21:47:05 +0100

    Update 2
    Dodanie poruszającego się losowo drzewa od lewej do prawej, z losową zmianą kierunku i prędkości powiązanej ze zmianą kierunku.

--- ApplePicker/Assets/Scripts/TreeController.cs
+++ ApplePicker/Assets/Scripts/TreeController.cs
@@ -5,26 +5,53 @@ using UnityEngine;
 public class TreeController : MonoBehaviour
 {
     public Rigidbody rb;
-    float speed;
-    public float x = 100;
-
+    float speed = 2.0f;
+    int direction = 0;
     public float time;
+    float changeRotation = 0.02f; //szansa na zmianê rotacji w trakcie jednego z kierunków przed osi¹gnieciem krawêdzi
+    float leftCorner = -8.0f;
+    float rightCorner = 8.0f;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
     void Update()
     {
-        speed = Random.Range(1, 8);
+        Vector3 position = transform.position;
+        position.x += speed * Time.deltaTime;
+        transform.position = position;
+
+        if (position.x < leftCorner)
+        {
+            randomSpeed();
+            speed = Mathf.Abs(speed);
+        }
+        else if (position.x > rightCorner)
+        {
+            randomSpeed();
+            speed = -Mathf.Abs(speed);
+        }
+        Debug.Log(speed);
+
+        //direction = Random.Range(-1, 2);
         InvokeRepeating("ThrowAnApple", 10f, 10f);
+    }
+
+    float randomSpeed()
+    {
+        speed = Random.Range(3.0f, 10.0f);
+        return speed;
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
-        rb.velocity = Vector3.right * speed;
+        if (Random.value < changeRotation)
+        {
+            randomSpeed();
+            speed *= -1;
+        }
     }
 
     void ThrowAnApple()
@@ -32,7 +59,7 @@ public class TreeController : MonoBehaviour
         time += Time.deltaTime;
         int clock = (int)time;
         //Debug.Log(clock);
-        Debug.Log("Throw apple");
-        Debug.Log(Time.time);
+        //Debug.Log("Throw apple");
+        //Debug.Log(Time.time);
     }
 }
